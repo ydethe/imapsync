e146cb77be9862229180cc9c42653c0c0bd6786f
Yann de The <ydethe@gmail.com>
2025-06-15 21:02:54 +0200

    Added docker secrets

--- src/imapsync/__main__.py
+++ src/imapsync/__main__.py
@@ -3,10 +3,12 @@ import schedule
 
 from .imap_sync import sync_all
 from .config import config
+from . import logger
 
 
 if __name__ == "__main__":
     schedule.every(config.SYNC_PERIOD).minutes.do(sync_all)
+    logger.info("Started imapsync")
 
     while True:
         schedule.run_pending()
